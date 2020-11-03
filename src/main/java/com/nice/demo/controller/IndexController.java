6e7e3505d8cf00e45984d69d764288527ddffe5a
Administrator <1442722749@qq.com>
2020-11-03 11:36:22 +0800

    get IP

--- src/main/java/com/nice/demo/controller/IndexController.java
+++ src/main/java/com/nice/demo/controller/IndexController.java
@@ -1,9 +1,33 @@
 package com.nice.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @GetMapping("/show")
+    @ResponseBody
+    public String getIP(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip.equals("0:0:0:0:0:0:0:1")) {
+            ip = "localhost";
+        }
+        System.out.println(ip);
+        return ip;
+    }
+
 
 
 }
