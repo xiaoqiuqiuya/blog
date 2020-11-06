8233d76b981e208be2a85685ea20addeeca8affd
nice <1442722749@qq.com>
2020-11-06 10:29:43 +0800

    login interceptor

---
+++ src/main/java/com/nice/demo/pojo/User.java
@@ -0,0 +1,74 @@
+package com.nice.demo.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 杨锦亮
+ * @since 2020-11-04
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="User对象", description="")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "账号")
+    private String userName;
+
+    @ApiModelProperty(value = "真实姓名")
+    private String fullName;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "上次登录时间")
+    private Date loginTime;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @ApiModelProperty(value = "头像地址")
+    private String avatarUrl;
+
+    @ApiModelProperty(value = "年龄")
+    private Integer age;
+
+    @ApiModelProperty(value = "地址")
+    private String addr;
+
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "手机")
+    private String phone;
+
+    @ApiModelProperty(value = "IP地址")
+    private String ipAddr;
+
+
+}
---
+++ src/main/java/com/nice/demo/service/UserService.java
@@ -0,0 +1,16 @@
+package com.nice.demo.service;
+
+import com.nice.demo.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 杨锦亮
+ * @since 2020-11-04
+ */
+public interface UserService extends IService<User> {
+
+}
